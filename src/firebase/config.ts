d5d3dd6916dace31b427550168dc4c71fbc86208
Grant Rizzo <grantrizzo2@gmail.com>
2026-01-27 07:01:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4293456336-4dfec",
+  "appId": "1:826364918923:web:061679ee7f8242b065a88e",
+  "apiKey": "AIzaSyD7_ndqb5OkEqX8awIJqNbqur1h4yNHQO8",
+  "authDomain": "studio-4293456336-4dfec.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "826364918923"
+};
