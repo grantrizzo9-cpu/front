2428e7ef39fe6b13c6fab6f9fe2c87cdcfb1b393
Grant Rizzo <grantrizzo2@gmail.com>
2026-01-27 17:40:37 +0000

    this

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:938676228347:web:2bf59812e3e56a641a27e0",
   "apiKey": "AIzaSyAZf04vj2gH1x0jJ7x-Z9v7X8s6Y5U2c3I",
   "authDomain": "studio-5024962673.firebaseapp.com",
-  "measurementId": "G-L1X1L2X1L2",
-  "messagingSenderId": "938676228347"
+  "storageBucket": "studio-5024962673.appspot.com",
+  "messagingSenderId": "938676228347",
+  "measurementId": "G-L1X1L2X1L2"
 };
