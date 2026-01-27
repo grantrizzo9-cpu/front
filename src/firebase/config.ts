d5fe2336acdec424f59a98050df62757f0b26e8b
Grant Rizzo <grantrizzo2@gmail.com>
2026-01-27 20:04:57 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBSiiuRvQ0zFO3rIdrdoYL9gdwiC8JROVQ",
-  authDomain: "studio-5024962673-4a760.firebaseapp.com",
-  projectId: "studio-5024962673-4a760",
-  storageBucket: "studio-5024962673-4a760.appspot.com",
-  messagingSenderId: "851224837187",
-  appId: "1:851224837187:web:2560c9ff938417f14968f6"
+  apiKey: "AIzaSyAz-3LwohmwM00hFfAC25Fq6t-9aGYqPzA",
+  authDomain: "studio-4293456336.firebaseapp.com",
+  projectId: "studio-4293456336",
+  storageBucket: "studio-4293456336.appspot.com",
+  messagingSenderId: "1090153835693",
+  appId: "1:1090153835693:web:96e0018503a55545a90d34"
 };
