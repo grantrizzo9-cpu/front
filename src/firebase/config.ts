e8ba25b78fe87dcc6229308f16cf80fa83f75f15
Grant Rizzo <grantrizzo2@gmail.com>
2026-01-28 18:36:12 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,4 @@
+
 // ACTION REQUIRED:
 // 1. Create a new Firebase project in the Firebase Console: https://console.firebase.google.com/
 // 2. In your new project, go to Project Settings (gear icon) -> Your apps.
@@ -8,8 +9,9 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAkUBf0vCY8KuF2OQ6M107mx8s1j9g7K1A",
   authDomain: "rent-a-host-96880149.firebaseapp.com",
   projectId: "rent-a-host-96880149",
-  storageBucket: "rent-a-host-96880149.firebasestorage.app",
+  storageBucket: "rent-a-host-96880149.appspot.com",
   messagingSenderId: "4112396981",
   appId: "1:4112396981:web:ae2ac3f35df96134f2c4fc",
   measurementId: ""
 };
+
